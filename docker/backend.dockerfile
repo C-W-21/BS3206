d6c7dba41e794cd823cb15a1702027f4b2aed88f
Callum W <ctw.odiham@gmail.com>
2024-04-20 19:34:14 +0100

    Fixed HMR on windows. Also started work on route planning. TODO - make it more dynamic

--- docker/backend.dockerfile
+++ docker/backend.dockerfile
@@ -5,4 +5,6 @@ COPY package*.json ./
 RUN npm i
 COPY . .
 EXPOSE 3000
-CMD ["npm", "run", "start"]
+
+ENTRYPOINT [ "npm" ]
+CMD ["run", "start"]
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,95 +1,36 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useEffect } from "react";
+import { Box, Button, Container } from "@mui/material";
+
+// Check connectivity to API
+function pingApi() {
+  fetch("/api/v1/ping").then((rsp) => {
+      if (rsp.status === 200) {
+        rsp.json().then((obj) => {
+          console.log(obj)
+        })
+      } else {
+        console.log(`Could not ping API, recieved code ${rsp.status}`)
+      }
+  }).catch((err) => {
+    console.log(`Could not ping API - ${err}`)
+  })
+}
 
 export default function Home() {
+  // Check API connectivity on page load
+  useEffect(pingApi, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          > 
-            By{" "}
-            <Image 
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Box 
+      sx={{
+        height: '100vh',
+        wodth: '100vw',
+        backgroundColor: 'darkgreen'
+      }}
+    >
+      <Button href="/route" variant="contained">Plan Route</Button>
+    </Box>
   );
 }
