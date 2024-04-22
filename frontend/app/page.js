ad73ef5e34fee13b44a612b08db82a8781c019ed
gweaving <118439982+gweaving@users.noreply.github.com>
2024-04-22 18:47:55 +0100

    Test

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -61,7 +61,10 @@ export default function Home() {
           <h2>
             Learn <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!
+
+            THIS IS A TEST
+          </p>
         </a>
 
         <a
