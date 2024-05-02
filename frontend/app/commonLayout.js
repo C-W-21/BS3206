6fbe9bc2b51acebe861ee362211da070152dd1ab
weaving_g <georgina.weaving@fujitsu.co.uk>
2024-05-02 09:42:36 +0100

    changes to login layout

--- frontend/app/commonLayout.js
+++ frontend/app/commonLayout.js
@@ -9,7 +9,9 @@ const CommonLayout = ({ title, children }) => {
             <Box display="flex" alignItems="center" width="80%" p={1} mt={1} mb={0}>
             <Button href="/" style={{ color: '#ffffff' }}>Home</Button>
             <Button href="/route" style={{ marginLeft: '10px', color: '#ffffff' }}>Plan Route</Button>
-            <Typography variant="h1" align="center" style={{ fontFamily: 'Helvetica', fontSize: '35px', marginLeft: '100px', flexGrow: 0.75 }}>{title}</Typography>
+            <Button href="/managementsearch" style={{ marginLeft: '10px', color: '#ffffff' }}>VMS</Button>
+            <Button href="/" style={{ marginLeft: '10px', color: '#ffffff' }}>VS</Button>
+            <Typography variant="h1" align="center" style={{ fontFamily: 'Helvetica', fontSize: '35px', flexGrow: 0.75 }}>{title}</Typography>
             <Button href="/login" style={{ marginLeft: 'auto', color: '#ffffff' }}>Sign Out</Button>
             </Box>
 
--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,18 +1,45 @@
 'use client'
 
-import { useEffect } from "react";
-import { Box, Button, Container, ThemeProvider } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Box, Button, TextField, ThemeProvider, Typography } from "@mui/material";
 import theme from "../theme";
-import LoginLayout from "../loginLayout";
+import "./validation";
 
 
 export default function Login() {
-    const pageTitle = "Login";
+  const [values, setValues] = useState({
+    username: '',
+    password: ''
+  })
   
-    return (
-      <ThemeProvider theme={theme}>
-        <LoginLayout title={pageTitle}>
-        </LoginLayout>
-      </ThemeProvider>
-    );
+  const handleInput =(event) => {
+    setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
   }
+
+  const handleSubmit =(event) => {
+    event.preventDefault();
+    setValues(validation(values));
+  }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box display="flex" flexDirection="column" alignItems="center" height="100vh" justifyContent="center">
+                <Box bgcolor="#f0f0f0" p={4} borderRadius={8} maxWidth={400} width="80%">
+                    <Typography variant="h1" align="center" style={{ color: '#000000', fontFamily: 'Helvetica' }} gutterBottom>Login</Typography>
+                    <form action="" onSubmit={handleSubmit}>
+                    <Box mb={2}>
+                        <TextField label="User" variant="outlined" name="username" onChange={handleInput} fullWidth />
+                    </Box>
+                    <Box mb={2}>
+                        <TextField label="Password" variant="outlined" name="password" onChange={handleInput} type="password" fullWidth/>
+                    </Box>
+                    <Box mb={2}>
+                        <Button variant="contained" href="/" color="primary" type="submit" fullWidth>Login</Button>
+                    </Box>
+                    <Button variant="outlined" href="/signup" style={{ color: '#000000' }} fullWidth>Sign Up</Button>
+                    </form>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );    
+}    
