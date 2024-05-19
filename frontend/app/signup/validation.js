0709216c001e1747d2339fe6a2ea4a093a628572
gweaving <118439982+gweaving@users.noreply.github.com>
2024-05-19 21:03:44 +0100

    upped to 8 character length

--- frontend/app/signup/validation.js
+++ frontend/app/signup/validation.js
@@ -10,12 +10,12 @@ function Validation(values) {
         error.username = "";
     }
 
-    // Validate password is  < 7 characters, 2 numbers and 1 special character
+    // Validate password is  < 8 characters, 2 numbers and 1 special character
     const password = values.password.toString().trim();
     if (password === "") {
         error.password = "Password should not be empty";
-    } else if (password.length < 7) {
-        error.password = "Password should be at least 7 characters long";
+    } else if (password.length < 8) {
+        error.password = "Password should be at least 8 characters long";
     } else if (!/\d{2}/.test(password)) {
         error.password = "Password should contain at least 2 numbers";
     } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
